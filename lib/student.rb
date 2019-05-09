ae501520135a584a860a126f0998f4be22d46767
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-09 00:35:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
-
+class Student < User 
+  
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn(string)
+    @knowledge << "Ruby framework Rails gem bundle update"
+  end 
+  
+  def knowledge
+    @knowledge << "Javascript Ember Elixir knowledge"
+    @knowledge
+  end
 end
