b9d21c5cd04d3b60848d5271d3783214ce134553
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-09 00:12:46 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,10 @@
 class User 
   
-  attr_accessor :first, :last 
+  attr_accessor :first_name, :last_name
   
   def initialize
-    @first = first 
-    @last = last 
+    @first_name = first_name
+    @last_name = last_name 
   end 
   
 end 
