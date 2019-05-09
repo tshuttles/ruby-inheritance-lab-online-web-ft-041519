54026c216db0a185814c79a0fd7097b1f6accf32
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-09 00:28:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,8 @@ class Student < User
     @knowledge = [] 
   end 
   
-  def learn(knowledge.)
-    @knowledge << "" 
+  def learn(string)
+    @knowledge << knowledge.sample 
   end 
   
   def knowledge
